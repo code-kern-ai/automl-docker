cb3e75617cd568d978452e46f9cfb4a24df6b24e
Johannes Hötter <jhoetter@MBPvonJohannes.fritz.box>
2022-05-23 22:14:34 +0200

    restructuring

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM python:3.8.10
- 
+
 WORKDIR /automl
 COPY . /automl
- 
+
 RUN pip install -r requirements.txt
 
 # Run api and models
-CMD ["uvicorn", "api:app", "--host", "0.0.0.0", "--port", "80"]
-
-
+CMD ["uvicorn", "api:api","--host", "0.0.0.0", "--port", "7531", "--reload", "--app-dir", "app/backend"]
--- app/backend/api.py
+++ app/backend/api.py
@@ -1,30 +1,35 @@
 # Import statements go here
-from urllib import response
-import uvicorn 
-import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
-from reuse_model import load_transformer, create_embeddings, create_predictions
+from reuse_model import (
+    load_transformer,
+    create_embeddings,
+    create_predictions,
+)
 
 # Instantiate fastapi app
-app = FastAPI()
+api = FastAPI()
+
 
 class Text(BaseModel):
     text: list
 
+
 # class Predictions(BaseModel):
 #     preds: dict
 
-@app.get("/")
+
+@api.get("/")
 def read_root():
     """
-    Simple greeting function. 
+    Simple greeting function.
     """
     return {"message": "Welcome to the Kern automl tool."}
 
-@app.post("/predict") #response_model=Predictions
+
+@api.post("/predict")  # response_model=Predictions
 def predict(data: Text):
-    """ 
+    """
     Get text data and returns predictions from a ml model.
     """
 
@@ -40,4 +45,3 @@ def predict(data: Text):
     # Use ml model to create predictions
     prediction = create_predictions(embeddings)
     return prediction.tolist()
-
